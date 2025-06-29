d80407aa6755d1183c573b51deb43904cdb602a7
Anas <anas.zulkifli2001@gmail.com>
2025-06-29 23:06:42 +0800

    feat: creat Col to put card

--- app/page.tsx
+++ app/page.tsx
@@ -11,60 +11,66 @@ import { Image } from "@heroui/image";
 
 export default function Home() {
   return (
-    <section className="flex flex-wrap  gap-4">
-      <div className="w-full lg:w-1/3">
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl"
-              src="https://heroui.com/images/hero-card-complete.jpeg"
-              width={270}
-            />
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <p className="text-tiny uppercase font-bold">Daily Mix</p>
-            <small className="text-default-500">12 Tracks</small>
-            <h4 className="font-bold text-large">Frontend Radio</h4>
-          </CardBody>
-        </Card>
-      </div>
+    <>
+      <section className="grid grid-cols-12  gap-4">
+        <div className="col-span-12 md:col-span-4 ">
+          <Card className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src="https://heroui.com/images/hero-card-complete.jpeg"
+                width={270}
+              />
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+              <p className="text-tiny uppercase font-bold">Daily Mix</p>
+              <small className="text-default-500">12 Tracks</small>
+              <h4 className="font-bold text-large">Frontend Radio</h4>
+            </CardBody>
+          </Card>
+        </div>
 
-      <div className="w-full lg:w-1/3">
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl"
-              src="https://heroui.com/images/hero-card-complete.jpeg"
-              width={270}
-            />
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <p className="text-tiny uppercase font-bold">Daily Mix</p>
-            <small className="text-default-500">12 Tracks</small>
-            <h4 className="font-bold text-large">Frontend Radio</h4>
-          </CardBody>
-        </Card>
-      </div>
-      <div className="w-full lg:w-1/3">
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl"
-              src="https://heroui.com/images/hero-card-complete.jpeg"
-              width={270}
-            />
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <p className="text-tiny uppercase font-bold">Daily Mix</p>
-            <small className="text-default-500">12 Tracks</small>
-            <h4 className="font-bold text-large">Frontend Radio</h4>
-          </CardBody>
-        </Card>
-      </div>
-    </section>
+        <div className="col-span-12 md:col-span-4 ">
+          <Card className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src="https://heroui.com/images/hero-card-complete.jpeg"
+                width={270}
+              />
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+              <p className="text-tiny uppercase font-bold">Daily Mix</p>
+              <small className="text-default-500">12 Tracks</small>
+              <h4 className="font-bold text-large">Frontend Radio</h4>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-span-12 md:col-span-4 ">
+          <Card className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src="https://heroui.com/images/hero-card-complete.jpeg"
+                width={270}
+              />
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+              <p className="text-tiny uppercase font-bold">Daily Mix</p>
+              <small className="text-default-500">12 Tracks</small>
+              <h4 className="font-bold text-large">Frontend Radio</h4>
+            </CardBody>
+          </Card>
+        </div>
+      </section>
+      <section>
+        <h1 className="text-center">Vortex Official Worldwide Store</h1>
+      </section>
+    </>
+
   );
 }
 
