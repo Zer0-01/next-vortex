843b9fb35991d5338e3156e30029cf1d8f7a03ca
Anas <anas.zulkifli2001@gmail.com>
2025-06-30 22:03:00 +0800

    feat: add vortex logo on navbar

--- components/vortex-navbar.tsx
+++ components/vortex-navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from "@heroui/button";
+import { Image } from "@heroui/image";
 import { Link } from "@heroui/link";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@heroui/navbar";
 import React from "react";
@@ -23,26 +24,21 @@ export default function App() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     const menuItems = [
-        "Profile",
-        "Dashboard",
-        "Activity",
-        "Analytics",
-        "System",
-        "Deployments",
-        "My Settings",
-        "Team Settings",
-        "Help & Feedback",
-        "Log Out",
+        "Home",
+        "Team",
+        "Shop",
+        "Contact Us",
     ];
 
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen}>
+        <Navbar onMenuOpenChange={setIsMenuOpen} >
             <NavbarContent>
                 <NavbarMenuToggle
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                     className="sm:hidden"
                 />
                 <NavbarBrand>
+                    <Image src="/images/image-vortex-logo.jpg" className="h-12" />
                     <p className="font-bold text-inherit">Vortex Academia</p>
                 </NavbarBrand>
             </NavbarContent>
@@ -50,30 +46,21 @@ export default function App() {
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
                     <Link color="foreground" href="#">
-                        Features
+                        Team
                     </Link>
                 </NavbarItem>
                 <NavbarItem isActive>
                     <Link aria-current="page" href="#">
-                        Customers
+                        Shop
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
                     <Link color="foreground" href="#">
-                        Integrations
+                        Contact Us
                     </Link>
                 </NavbarItem>
             </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
+
             <NavbarMenu>
                 {menuItems.map((item, index) => (
                     <NavbarMenuItem key={`${item}-${index}`}>
