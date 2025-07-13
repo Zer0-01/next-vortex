0330568f606bb7b9b34af503454f21266519ddfb
Anas <anas.zulkifli2001@gmail.com>
2025-07-13 23:15:36 +0800

    feat: implement the style

--- components/headline.tsx
+++ components/headline.tsx
@@ -1,6 +1,6 @@
 function Headline() {
   return (
-    <h1 className="text-5xl text-center font-bold">
+    <h1 className="text-5xl text-center font-bold bg-">
       Where Passion Meets Precision, On and Off the Field
     </h1>
   );
--- components/vortex-team-image.tsx
+++ components/vortex-team-image.tsx
@@ -4,7 +4,11 @@ import imagevortexbanner from "../public/images/image-vortex-banner.jpg";
 
 function VortexTeamImage() {
   return (
-    <Image alt="HeroUI hero Image" src={imagevortexbanner.src} width={300} />
+    <Image
+      alt="HeroUI hero Image"
+      className="w-full"
+      src={imagevortexbanner.src}
+    />
   );
 }
 
