e02d15f2916f2c62adec3096bf300cd7dcf0fcce
Anas <anas.zulkifli2001@gmail.com>
2025-09-29 22:22:37 +0800

    feat: delete

--- components/headline.tsx
+++
@@ -1,9 +0,0 @@
-function Headline() {
-  return (
-    <h1 className="text-5xl text-center font-bold bg-">
-      Where Passion Meets Precision, On and Off the Field
-    </h1>
-  );
-}
-
-export default Headline;
--- components/social-media.tsx
+++
@@ -1,9 +0,0 @@
-function SocialMedia() {
-  return (
-    <>
-      <h2>Follow Vortex Academia on social media</h2>
-    </>
-  );
-}
-
-export default SocialMedia;
