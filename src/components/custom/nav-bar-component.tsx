936bfd7b6ef67fe51ef218f3fa6ef0b350039b65
Anas <anas.zulkifli2001@gmail.com>
2025-09-29 23:09:12 +0800

    feat: coplete nav bar

---
+++ src/components/custom/nav-bar-component.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "../ui/button";
+import {
+    NavigationMenu,
+
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+} from "../ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Menu } from "lucide-react";
+
+const NavbarComponent = () => {
+    return (
+        <header className="border-b bg-white">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
+                {/* Brand */}
+                <div className="text-xl font-bold">Vortex Academia</div>
+
+                {/* Desktop Navigation */}
+                <NavigationMenu className="hidden md:flex">
+                    <NavigationMenuList className="flex space-x-4">
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/">Home</NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/our-team">Our Team</NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/our-athletes">Our Athletes</NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+
+                {/* Mobile Hamburger */}
+                <div className="md:hidden">
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="ghost">
+                                <Menu size={24} />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="right" className="w-64">
+                            <NavigationMenu orientation="vertical">
+                                <NavigationMenuList className="flex flex-col space-y-4">
+                                    <NavigationMenuItem>
+                                        <NavigationMenuLink href="/">Home</NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuLink href="/our-team">Our Team</NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuLink href="/our-athletes">Our Athletes</NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                </NavigationMenuList>
+                            </NavigationMenu>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default NavbarComponent;
