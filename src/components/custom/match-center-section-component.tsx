a7bab42c24f1d6ce3d0509ad2b9a72d8150bc43f
Anas <anas.zulkifli2001@gmail.com>
2025-10-17 00:45:38 +0800

    feat: inprogress match center

--- src/components/custom/match-center-section-component.tsx
+++ src/components/custom/match-center-section-component.tsx
@@ -1,3 +1,6 @@
+import { Button } from "../ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+
 const MatchCenterSectionComponent = () => {
     return (
         <div className="grid grid-cols-12 p-6">
@@ -10,7 +13,31 @@ const MatchCenterSectionComponent = () => {
                     Stay updated with Vortex&apos;s latest matches, scores, and upcoming fixtures
 
                 </div>
+                <Card className="w-full bg-blue-400 border-0 ">
+                    <CardHeader>
 
+                        <CardAction>
+                            <div>Friendly Match</div>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent className="flex flex-row items-center">
+                        <div className="grow-1 items-center">Vortex</div>
+                        <div className="grow-1 flex flex-col text-center">
+                            <div> 2-2</div>
+                            <div>90</div>
+
+                        </div>
+                        <div className="grow-1 text-end">Vortex</div>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
             </div>
             <div></div>
         </div>
