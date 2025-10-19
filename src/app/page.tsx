927a2ee057fb6364962260a8830a6fb444187de4
Anas <anas.zulkifli2001@gmail.com>
2025-10-19 21:12:52 +0800

    feat: complete the squad showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ImageHeadlineComponent from "@/components/custom/image-headline-component";
 import MatchCenterSectionComponent from "@/components/custom/match-center-section-component";
+import SquadShowcaseComponent from "@/components/custom/squad-showcase-component";
 
 export default function Home() {
   return (
     <div>
       <ImageHeadlineComponent />
       <MatchCenterSectionComponent />
+      <SquadShowcaseComponent />
     </div>
 
   );
