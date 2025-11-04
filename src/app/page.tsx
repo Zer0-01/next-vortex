dda9653b197fbb9d2cd215257d07de86b7a604bd
Anas <anas.zulkifli2001@gmail.com>
2025-11-05 00:32:55 +0800

    feat: update for the about us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import AboutUsSectionComponent from "@/components/custom/about-us-section-component";
 import HeadlineComponent from "@/components/custom/headline-component";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
       <HeadlineComponent />
+      <AboutUsSectionComponent />
 
     </div>
 
---
+++ src/components/custom/about-us-section-component.tsx
@@ -0,0 +1,17 @@
+const AboutUsSectionComponent = () => {
+    return (
+        <div className="flex flex-col bg-white mt-16 items-center ">
+            <h1 className="text-black text-3xl text-center pt-8 px-8">
+                Born from Friendship, United by Passion
+            </h1>
+
+            <h2 className="text-black  text-center pt-8 px-8">
+                What started as a simple way to bond among friends during our school and university days has grown into something much more.
+                We’re a team built on trust, laughter, and shared memories — where every match reminds us why we play: for the love of the game and the brotherhood that comes with it.
+                Our journey continues, not just as teammates, but as lifelong friends chasing the same passion together.
+            </h2>
+        </div>
+    )
+}
+
+export default AboutUsSectionComponent
