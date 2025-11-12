022654bf75d49d015d64f3607208211e987c9566
Anas <anas.zulkifli2001@gmail.com>
2025-11-12 20:59:21 +0800

    feat: update card

--- src/components/custom/product-section-component.tsx
+++ src/components/custom/product-section-component.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
 import jersey1 from "../../../public/image-jersey.png"
-import jersey2 from "../../../public/image-jersey.png"
 
 
 const ProductSectionComponent = () => {
@@ -10,39 +9,46 @@ const ProductSectionComponent = () => {
             <h1 className="text-white text-3xl text-center ">
                 Product
             </h1>
-            <div className="flex gap-4 w-full">
-                <Card className="grow" >
-                    <CardHeader>
-                        <CardTitle>
-                            <div className="relative w-full max-w-3xl h-64 sm:h-80 md:h-[500px]">
-                                <Image
-                                    src={jersey1}
-                                    alt="about us image"
-                                    fill
-                                    className="object-contain"
-                                    priority
-                                />
-                            </div>                        </CardTitle>
 
-                    </CardHeader>
-                    <CardContent className="text-center">
-                        <p>2024/2025</p>
-                    </CardContent>
-                   
-                </Card>
-                <Card className="grow" >
-                    <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
-                    </CardContent>
-                    <CardFooter>
-                        <p>Card Footer</p>
-                    </CardFooter>
-                </Card>
-            </div>
+            <Card className="w-full" >
+                <CardHeader>
+                    <CardTitle>
+                        <div className="relative w-full max-w-3xl h-64 sm:h-80 md:h-[500px]">
+                            <Image
+                                src={jersey1}
+                                alt="about us image"
+                                fill
+                                className="object-contain"
+                                priority
+                            />
+                        </div>                        </CardTitle>
+
+                </CardHeader>
+                <CardContent className="text-center">
+                    <p>2024/2025</p>
+                </CardContent>
+
+            </Card>
+            <Card className="w-full" >
+                <CardHeader>
+                    <CardTitle>
+                        <div className="relative w-full max-w-3xl h-64 sm:h-80 md:h-[500px]">
+                            <Image
+                                src={jersey1}
+                                alt="about us image"
+                                fill
+                                className="object-contain"
+                                priority
+                            />
+                        </div>                        </CardTitle>
+
+                </CardHeader>
+                <CardContent className="text-center">
+                    <p>2024/2025</p>
+                </CardContent>
+
+            </Card>
+
 
 
         </div>
