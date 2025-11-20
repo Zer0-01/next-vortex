cf1da75b044755446257755379fdae8d98ead5dc
Anas <anas.zulkifli2001@gmail.com>
2025-11-20 17:14:20 +0800

    feat: products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeadlineComponent from "@/app/components/headline-component";
 import OurTeamSectionComponent from "./components/our-team-section-component";
 import TeamSectionComponent from "./components/team-section-component";
 import MatchCenterSectionComponent from "./components/match-center-section-component";
+import ProductSectionComponent from "./components/product-section-component";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <MatchCenterSectionComponent />
       <OurTeamSectionComponent />
       <TeamSectionComponent />
+      <ProductSectionComponent />
 
 
 
